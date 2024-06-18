cca636bd931fcec2f49b5e129acfb30c15f66acb
Blaine Motsinger <blaine@renderorange.com>
2024-06-17 22:21:07 -0400

    Add functionality to apply patches in apply_database_patches.bash
    Additionally set error mode in create_database.bash.

--- bin/apply_database_patches.bash
+++ bin/apply_database_patches.bash
@@ -1,4 +1,14 @@
 #/bin/bash
 
-echo "not yet implemented"
-exit;
+set -e
+
+DB_FILEPATH='db/remoteperljobs.sqlite3'
+
+for patch in $(ls -1 db/schema/); do
+	exists=$(sqlite3 $DB_FILEPATH "select count(*) from db_patch_history where patch_name = '$patch'")
+	if [ "$exists" -eq "0" ]; then
+		echo "applying $patch"
+		sqlite3 $DB_FILEPATH < db/schema/$patch
+		sqlite3 $DB_FILEPATH "insert into db_patch_history (patch_name, applied_on) values ('$patch', $(date +%s))"
+	fi
+done
--- bin/create_database.sh
+++ bin/create_database.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+set -e
+
 DB_FILEPATH='db/remoteperljobs.sqlite3'
 if [ -f $DB_FILEPATH ]; then
 	echo "$DB_FILEPATH exists"
